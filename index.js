afe1179eada61f5c4519b6adad81d00f8a95b65a
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-11-04 16:19:56 +0100

    worked on js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-document.getElementById('hero').innerHTML = `
+const hero = {
+    elementId : 'hero',
+    name : 'Wizard',
+    avatar : '/images/wizard.png',
+    health : '60',
+    diceRoll : 6
+}
+
+const monster = {
+    elementId : 'monster',
+    name : 'Orc',
+    avatar : '/images/orc.png',
+    health : '10',
+    diceRoll : 4
+}
+
+function renderCharacter(elementId,name,avatar,health,diceRoll){
+    document.getElementById(elementId).innerHTML = `
         <div class="character-card">
-            <h4 class="name"> Wizard </h4>
-            <img class="avatar" src="images/wizard.png"/>
-            <p class="health">health: <b> 60 </b></p>
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}"/>
+            <p class="health">health: <b> ${health} </b></p>
             <div class="dice-container">
-            <div class="dice"> 6 </div>
+            <div class="dice"> ${diceRoll} </div>
             </div>
         </div>        
 `
+}
 
-document.getElementById('monster').innerHTML = `
-        <div class="character-card">
-            <h4 class="name"> Orc </h4>
-            <img class="avatar" src="images/orc.png"/>
-            <p class="health">health: <b> 10 </b></p>
-            <div class="dice-container"><div class="dice"> 4 </div></div>
-        </div>
-`
+renderCharacter(heroElementId,heroName,heroAvatar,heroHealth,heroDiceRoll)
+renderCharacter(monsterElementId,monsterName,monsterAvatar,monsterHealth,monsterDiceRoll)
